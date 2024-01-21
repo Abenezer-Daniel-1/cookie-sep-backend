8118ecefda7b1b7c1924350ad4750a304748d61e
Abenezer-Daniel-1 <abenezerdanieldesalegn@gmail.com>
2024-01-21 14:03:23 +0200

    new react app

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,10 +1,6 @@
 class HomeController < ApplicationController
   def index
-    # cookies[:message] = "This is the true test. For production cookies"
-    cookies[:message] = {
-      value: "this is the cookie with expiration date.",
-      expires: 30.minutes.from_now
-    }
+    cookies[:message] = "This is the true test. For production cookies"
 
     render json: {
       message: cookies[:message]
