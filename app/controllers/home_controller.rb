df1c78a2a460a98a6d656522f44d47251ba830f3
Abenezer-Daniel-1 <abenezerdanieldesalegn@gmail.com>
2024-01-21 13:01:11 +0200

    frontend connect

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,6 +1,8 @@
 class HomeController < ApplicationController
   def index
     cookies[:message] = "This is the true test. For production cookies"
-    render json: cookies[:message]
+    render json: {
+      message: cookies[:message]
+    }
   end
 end
