40d3a18a268548cfd4b620b19a0269d1b2f50fde
Abenezer-Daniel-1 <110535707+Abenezer-Daniel-1@users.noreply.github.com>
2024-01-21 14:35:06 +0200

    final

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -5,7 +5,7 @@ class HomeController < ApplicationController
     cookies[:message] = {
     value: 'your_cookie_value',
     expires: 30.days.from_now, # Set an expiration time (e.g., 30 days from now)
-    domain: 'your-domain.com', # Set the domain of your Vue.js frontend
+    domain: 'https://cookie-sep-frontend.vercel.app', # Set the domain of your Vue.js frontend
     secure: true, # Set to true if using HTTPS
     same_site: :none # Allow cross-site cookie sharing
     }
