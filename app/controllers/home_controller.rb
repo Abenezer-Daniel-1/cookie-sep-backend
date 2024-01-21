0834ae9bd4bc06a11dd84b36eb1ab0a1bb99959e
Abenezer-Daniel-1 <abenezerdanieldesalegn@gmail.com>
2024-01-21 13:25:16 +0200

    frontend connect

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,6 +1,11 @@
 class HomeController < ApplicationController
   def index
-    cookies[:message] = "This is the true test. For production cookies"
+    # cookies[:message] = "This is the true test. For production cookies"
+    cookies[:message] = {
+      value: "this is the cookie with expiration date.",
+      expires: 30.minutes.from_now
+    }
+
     render json: {
       message: cookies[:message]
     }
